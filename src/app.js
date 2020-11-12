6f9567d5b479ba6e0d939845d272ab72dca234db
pratik mane <pratik.mane1299@gmail.com>
2020-11-12 21:20:35 +0530

    add product images & serve static files

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const middlewares = require('./middlewares');
@@ -14,6 +15,8 @@ app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' }));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.json({
